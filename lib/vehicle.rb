5d148c5a285155de87781fe27e4ac685dd304e37
Jennifer Prince <jennifergraceprince@gmail.com>
2020-06-15 01:36:29 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,10 @@
 class Vehicle
-
+  attr_accessor :wheel_sizer, :wheel_number
+  
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+  
+  def go ""
 end
